03d3bd76a3a11486f7feada58a742e54e66d39f7
BalajiGopal <BalajiGopal@Balaji-Gopal.local>
2021-10-15 13:17:05 +0530

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-starter';
+  public menuContent: boolean = false;
+  public show:boolean = true;
+  public hide:boolean = false;
+  public year:any = new Date().getFullYear();
+
+  openMenu() {
+    this.menuContent = !this.menuContent;
+  }
+}
